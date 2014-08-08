115c2f299b102849e393319ea3d5988f4ca524a0
AlanRosenthal <alan.rosenthal@gmail.com>
2014-08-08 16:04:33 -0400

    tweaking

--- javascripts/github_issues.js
+++ javascripts/github_issues.js
@@ -1,12 +1,19 @@
 console.log("tesT");
 
-xmlhttp=new XMLHttpRequest();
+var xmlhttp=new XMLHttpRequest();
+var json;
 xmlhttp.open("GET","https://api.github.com/repos/iSENSEDev/rSENSE/issues?labels=In+Testing",true);
 xmlhttp.send();
 xmlhttp.onreadystatechange=function()
 {
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-    console.log(xmlhttp.responseText);
+//        console.log(xmlhttp.responseText);
+        json = xmlhttp.resonseText;
+        runthis();
     }
+}
+
+function runthis() {
+    console.log(json);
 }
