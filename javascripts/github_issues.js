a2668d1a654b3062a12b863e7ecb2b7ca397d309
AlanRosenthal <alan.rosenthal@gmail.com>
2014-08-08 16:00:35 -0400

    added xml request

--- javascripts/github_issues.js
+++ javascripts/github_issues.js
@@ -1,1 +1,12 @@
 console.log("tesT");
+
+xmlhttp=new XMLHttpRequest();
+xmlhttp.open("GET","https://api.github.com/repos/iSENSEDev/rSENSE/issues?labels=In+Testing",true);
+xmlhttp.send();
+xmlhttp.onreadystatechange=function()
+{
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    console.log(xmlhttp.responseText);
+    }
+}
